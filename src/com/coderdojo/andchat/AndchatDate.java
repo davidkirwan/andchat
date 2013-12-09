79a61e698aa8c8b28615d237a0517b9771cd5879
David Kirwan <dkirwan@tssg.org>
2013-12-09 08:56:05 +0000

    - Fixed error I introduced during the last merge ;/

--- src/com/coderdojo/andchat/AndchatDate.java
+++ src/com/coderdojo/andchat/AndchatDate.java
@@ -1,31 +1,14 @@
 package com.coderdojo.andchat;
 
 public class AndchatDate {
-<<<<<<< HEAD
+
 	private String year;
 	private String month;
 	private String day;
 	private String hour;
 	private String minute;
-	private String seconds;
-	
-	
-	public AndchatDate(String second2, String minute2, String hour2, String year2, String month2, String day2) {
-		year = year2;
-		month = month2;
-	}
-	
-	public String getDateTime() {
-		return getYear() + "/" + getMonth() + "/" + getDay() + " " + getHours() + ":" + getMinutes();
-=======
-	
-	private String year;
-	private String month;
-	private String day;
-	
-	private String minute;
-	private String hour;
 	private String second;
+	
 	
 	public AndchatDate(String second2, String minute2, String hour2, String year2, String month2, String day2) {
 		year = year2;
@@ -38,24 +21,17 @@ public class AndchatDate {
 	
 	public String getDateTime() {
 		return getYear() + "/" + getMonth() + "/" + getDay() + " " + getHours() + ":" + getMinutes();
->>>>>>> 75c6b0e2043a820054e78cfbb5a4924c1d8e992e
 	}
 	
 	public String getYear() {
 		return year;
 	}
-<<<<<<< HEAD
+
 	public void setYear(String year) {
 		this.year = year;
 	}
-	public String getMonth() {
-		return month;
-	}
 	public void setMonth(String month) {
 		this.month = month;
-	}
-	public String getDay() {
-		return day;
 	}
 	public void setDay(String day) {
 		this.day = day;
@@ -71,15 +47,7 @@ public class AndchatDate {
 	}
 	public void setMinute(String minute) {
 		this.minute = minute;
-	}
-	public String getSeconds() {
-		return seconds;
-	}
-	public void setSeconds(String seconds) {
-		this.seconds = seconds;
-	}
-=======
-	
+	}	
 	public String getMonth() {
 		return month;
 	}
@@ -99,6 +67,4 @@ public class AndchatDate {
 	public String getSeconds() {
 		return second;
 	}
-	
->>>>>>> 75c6b0e2043a820054e78cfbb5a4924c1d8e992e
 }
