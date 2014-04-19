696c5dea53f4c880acb69e4bac46a67a5442df70
ByteFel <liamfpp@gmail.com>
2014-04-19 14:14:40 +0100

    Remove unused imports.

--- src/com/coderdojo/libretalk/LibretalkMessageData.java
+++ src/com/coderdojo/libretalk/LibretalkMessageData.java
@@ -7,9 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.io.StreamCorruptedException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 import android.graphics.Color;
 import android.util.Log;
